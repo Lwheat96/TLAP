53c170b4283370b6fcbb7986b7c9c19375114b6b
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-14 15:54:21 -0400

    I only needed a different equation in the loop, not an if statement

--- Chapter 2: Puzzles/First-Puzzle.cpp
+++ Chapter 2: Puzzles/First-Puzzle.cpp
@@ -2,9 +2,15 @@
 #include <vector>
 
 int main() {
+for (int row = 1; row <= 5; row++) {
     for (int hashNum = 1; hashNum <= 5; hashNum++) {
         std::cout << "#";
+        /*if (row < 5; hashNum--){
+
+            std::cout << hashNum--;
+        }*/
     }
     std::cout << "\n";
-    std::cout << "The first line is showing correctly.\n";
+}
+    //std::cout << "The first line is showing correctly.\n";
 }
