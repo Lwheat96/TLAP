64dbf4fba67a077c8c8556e4bd34736afe74f456
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-14 12:02:14 -0400

    I honestly don't know what I am currently committing

--- Chapter 2: Puzzles/First-Puzzle.cpp
+++ Chapter 2: Puzzles/First-Puzzle.cpp
@@ -5,5 +5,6 @@ int main() {
     for (int hashNum = 1; hashNum <= 5; hashNum++) {
         std::cout << "#";
     }
-    std::cout << "/n";
+    std::cout << "\n";
+    std::cout << "The first line is showing correctly.\n";
 }
