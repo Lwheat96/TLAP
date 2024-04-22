b851b6c307a9eabe16a32662f140868a2c3087e3
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-21 20:47:38 -0400

    Just catching up on some reading of Think like a programmer

--- Chapter 2: Puzzles/First-Puzzle.cpp
+++ Chapter 2: Puzzles/First-Puzzle.cpp
@@ -5,7 +5,7 @@ int main() {
 for (int row = 1; row <= 5; row++) {
     for (int hashNum = 1; hashNum <= 6 - row; hashNum++) {
         std::cout << "#";
-    }
+    } 
     std::cout << "\n";
 }
     //std::cout << "The first line is showing correctly.\n";
