2a0c2051984256b0201d9c5817bb4ea3c9945db7
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-23 08:49:29 -0400

    I diagramed out what I needed the program to look like.

--- Chapter 2: Puzzles/second-puzzle.cpp
+++ Chapter 2: Puzzles/second-puzzle.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <vector>
+/*  I need there to be a program that looks like this.
 
+#
+##
+###
+####
+###
+##
+#
+
+*/
 int main() {
 for (int row = 1; row <= 5; row++) {
     for (int hashNum = 1; hashNum <= 6 - row; hashNum++) {
