8a217a1ce99035648ad1a97c98db969638be1a8f
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-23 08:51:10 -0400

    I compiled second-puzzle

--- Chapter 2: Puzzles/second-puzzle.cpp
+++ Chapter 2: Puzzles/second-puzzle.cpp
@@ -12,7 +12,7 @@
 
 */
 int main() {
-for (int row = 1; row <= 5; row++) {
+for (int row = 1; row <= 6; row++) {
     for (int hashNum = 1; hashNum <= 6 - row; hashNum++) {
         std::cout << "#";
     } 
