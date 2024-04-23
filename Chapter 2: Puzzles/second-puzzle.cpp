b35b4ccd6dd7b182f4910581f06095f9d4bc8b13
lukewheat <158610935+Lwheat96@users.noreply.github.com>
2024-04-23 11:48:51 -0400

    Working out the ideas for what I need to code.

--- Chapter 2: Puzzles/second-puzzle.cpp
+++ Chapter 2: Puzzles/second-puzzle.cpp
@@ -11,8 +11,10 @@
 #
 
 */
+
+//Instead of subtracting for the first 3 rows we need to add?
 int main() {
-for (int row = 0; row <= 6; row++) {
+for (int row = 1; row <= 7; row++) {
     for (int hashNum = 1; hashNum <= 6 - row; hashNum++) {
         std::cout << "#";
     } 
